86c48792421b0aae21edd30da29ab701f078b49b
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-11-12 16:02:42 -0500

    basic page made with lorum ipsum

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Particles from "./Particles";
+
+// Render home and particle effects
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Particles />
+    </React.Fragment>
+  );
+};
+
+export default Home;
